795dff8927c850e66ca2339fd2dc2473e2696451
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2024-12-04 15:02:52 +0530

    Updated based on second review

--- backend/server.js
+++ backend/server.js
@@ -9,9 +9,7 @@ const PORT = process.env.PORT || 5173;
 const FRONTEND_URL = process.env.FRONTEND_URL || `http://localhost:${PORT}`;
 
 app.use(express.json());
-app.use(cors({
-    origin:  FRONTEND_URL
-}));
+app.use(cors());
 
 app.get('/', (req, res)=>{
     res.status(200).send("Response");
@@ -21,5 +19,5 @@ app.get('/ping', (req, res)=>{
 })
 
 app.listen(PORT, ()=>{
-    console.log(`Server Listening on http://localhost:${PORT}`);
+    console.log(`Server Listening on ${FRONTEND_URL}`);
 })
