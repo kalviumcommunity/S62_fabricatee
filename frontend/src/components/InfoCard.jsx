bad371c829b147cb47523564bbd20cdcd01a31af
Joshua Jinu <116093853+joshua-jinu@users.noreply.github.com>
2025-01-26 22:54:13 +0530

    updated inforcard in homepage

--- frontend/src/components/InfoCard.jsx
+++ frontend/src/components/InfoCard.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const InfoCard = ({ imageUrl, title, description }) => {
   return (
-    <div className="bg-white shadow-lg rounded-lg w-80 mx-auto">
+    <div className="bg-white shadow-lg rounded-lg w-80 m-4 mx-auto">
       <img src={imageUrl} alt={title} className="w-full max-h-56 mx-auto mb-4" />
       <div className="p-6">
         <h3 className="text-xl font-semibold text-center mb-2">{title}</h3>
